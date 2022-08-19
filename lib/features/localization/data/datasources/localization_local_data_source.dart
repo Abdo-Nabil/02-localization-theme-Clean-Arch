2bb34428ff33e59f542f32f8de49f939f66dc4eb
Abdo Nabil <abdo.sw.eng@gmail.com>
2022-08-19 23:59:40 +0200

    first commit

---
+++ lib/features/localization/data/datasources/localization_local_data_source.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'dart:ui';
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:waslny_user/features/localization/data/models/locale_model.dart';
+import 'package:waslny_user/features/localization/locale/app_localizations_setup.dart';
+
+import '../../../../core/error/exceptions.dart';
+import '../../../../resources/strings_manager.dart';
+
+abstract class LocalizationLocalDataSource {
+  LocaleModel getLocale();
+  Future<LocaleModel> setToSystemLocale();
+  Future<Unit> setLocale(LocaleModel localeModel);
+}
+
+class LocalizationLocalDataSourceImpl implements LocalizationLocalDataSource {
+  final SharedPreferences sharedPreferences;
+  const LocalizationLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  LocaleModel getLocale() {
+    final String? jsonString =
+        sharedPreferences.getString(StringsManager.storedLocale);
+    if (jsonString != null) {
+      String decodedJsonData = json.decode(jsonString);
+      return LocaleModel(locale: Locale(decodedJsonData));
+    }
+    //
+    else {
+      return _setSystemLocaleIfFound();
+    }
+  }
+
+  @override
+  Future<LocaleModel> setToSystemLocale() async {
+    final bool result = await sharedPreferences.remove(
+      StringsManager.storedLocale,
+    );
+    if (result == false) {
+      throw CacheSavingException();
+    }
+    return _setSystemLocaleIfFound();
+  }
+
+  @override
+  Future<Unit> setLocale(LocaleModel localeModel) async {
+    final bool result = await sharedPreferences.setString(
+      StringsManager.storedLocale,
+      json.encode(localeModel.locale.languageCode),
+    );
+    if (result == false) {
+      throw CacheSavingException();
+    }
+    return Future.value(unit);
+  }
+
+  //The system (mobile) language
+  Locale _getDeviceLocale() {
+    final String result = Platform.localeName; //like en_US or ar_EG
+    final String localeString = result.split('_').first;
+    return Locale(localeString);
+  }
+
+  LocaleModel _setSystemLocaleIfFound() {
+    final Locale deviceLocal = _getDeviceLocale();
+    Locale systemLocaleOrEnglish =
+        AppLocalizationsSetup.localeResolutionCallback(
+            deviceLocal, AppLocalizationsSetup.supportedLocales);
+    //
+    return LocaleModel(locale: systemLocaleOrEnglish);
+  }
+}
---
+++ lib/features/theme/data/datasources/theme_local_data_source.dart
@@ -0,0 +1,59 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:waslny_user/features/theme/data/models/theme_model.dart';
+
+import '../../../../resources/strings_manager.dart';
+
+abstract class ThemeLocalDataSource {
+  ThemeModel getTheme();
+  Future<Unit> setTheme(ThemeModel themeModel);
+}
+
+class ThemeLocalDataSourceImpl implements ThemeLocalDataSource {
+  final SharedPreferences sharedPreferences;
+  const ThemeLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  ThemeModel getTheme() {
+    final bool? isLightTheme =
+        sharedPreferences.getBool(StringsManager.isLightTheme);
+    if (isLightTheme != null) {
+      if (isLightTheme) {
+        return const ThemeModel(themeMode: ThemeMode.light);
+      } else {
+        return const ThemeModel(themeMode: ThemeMode.dark);
+      }
+    }
+    //
+    else {
+      return const ThemeModel(themeMode: ThemeMode.system);
+    }
+  }
+
+  @override
+  Future<Unit> setTheme(ThemeModel themeModel) async {
+    switch (themeModel.themeMode) {
+      //
+      case ThemeMode.system:
+        {
+          await sharedPreferences.remove(StringsManager.isLightTheme);
+        }
+        break;
+      //
+      case ThemeMode.light:
+        {
+          await sharedPreferences.setBool(StringsManager.isLightTheme, true);
+        }
+        break;
+      //
+      case ThemeMode.dark:
+        {
+          await sharedPreferences.setBool(StringsManager.isLightTheme, false);
+        }
+        break;
+      //
+    }
+    return Future.value(unit);
+  }
+}
